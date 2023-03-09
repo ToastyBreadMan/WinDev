13d383d410796994f1f9acf99674300abaad5707
ToastyBreadMan <maclean.sam1997@gmail.com>
2023-03-09 15:52:27 -0500

    Backwords compatibility for Invoke-Webrequest

--- setup.ps1
+++ setup.ps1
@@ -15,7 +15,7 @@ function Update-Environment-Path {
 
 # get latest download url
 $URL = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
-$URL = (Invoke-WebRequest -Uri $URL).Content | ConvertFrom-Json |
+$URL = (Invoke-WebRequest -UseBasicParsing -Uri $URL).Content | ConvertFrom-Json |
         Select-Object -ExpandProperty "assets" |
         Where-Object "browser_download_url" -Match '.msixbundle' |
         Select-Object -ExpandProperty "browser_download_url"
