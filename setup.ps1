ae52d346fe533006c4b93bc4694afbf93bcb3a18
ToastyBreadMan <maclean.sam1997@gmail.com>
2023-03-09 17:28:59 -0500

    Now can download dependencies for winget

--- setup.ps1
+++ setup.ps1
@@ -8,6 +8,23 @@ function Update-Environment-Path {
 }
 
 
+# 
+# Dependencies
+#
+# Source: https://github.com/microsoft/winget-cli/issues/1861
+
+# Get 'Desktop Installer'
+Add-AppxPackage -Path https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx
+
+# Get Microsoft.UI.Xaml
+# Note: nuget is not owned by Microsoft, but is funded by microsoft.:shrug:
+Invoke-WebRequest -Uri https://www.nuget.org/api/v2/package/Microsoft.UI.Xaml/2.7.3 -OutFile .\microsoft.ui.xaml.2.7.3.zip
+Expand-Archive .\microsoft.ui.xaml.2.7.3.zip
+Add-AppxPackage .\microsoft.ui.xaml.2.7.3\tools\AppX\x64\Release\Microsoft.UI.Xaml.2.7.appx
+
+Remove-Item "microsoft.ui.xaml.2.7.3.zip"
+Remove-Item "microsoft.ui.xaml.2.7.3" -Recurse
+
 #
 # Winget
 #
@@ -28,3 +45,5 @@ Add-AppxPackage -Path "Setup.msix"
 
 # delete file
 Remove-Item "Setup.msix"
+
+Update-Environment-Path
